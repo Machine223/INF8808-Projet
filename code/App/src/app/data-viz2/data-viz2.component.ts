74baf0925d469216f273983e6a46fe41b1e27b50
Abderrahim Ammour <abderrahim223@gmail.com>
2022-04-05 06:35:50 -0400

    wip v2

--- code/App/src/app/data-viz2/data-viz2.component.ts
+++ code/App/src/app/data-viz2/data-viz2.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as d3 from 'd3';
+import { axisBottom, axisLeft, max, scaleBand, scaleLinear } from 'd3';
 
 @Component({
   selector: 'app-data-viz2',
@@ -6,10 +8,82 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-viz2.component.css']
 })
 export class DataViz2Component implements OnInit {
+  private data: any[] = [];
+  private svg: any;
+  private width: number = 800;
+  private height: number = 800;
+  private rows: number = 1;
+  private cols: number = 1;
+
+  selectedCategory: any;
+  categories: any[] = [
+    { value: 'canada', viewValue: 'Joueur Canadien' },
+    { value: 'concacaf', viewValue: 'Joueur de la Concacaf' },
+    { value: 'formation', viewValue: 'Afficher le club de formation' },
+    { value: 'club', viewValue: 'Afficher le club de actuelle' },
+  ];
+
 
   constructor() { }
 
   ngOnInit(): void {
+    d3.json('../../assets/data_vis2.json')
+      .then((data: any) => {
+        this.data = data;
+        // console.table(this.data);
+        // this.sortPlayers('MP'); // sort player by default mode
+      })
+      .then(() => {
+        this.createSvg();
+      });
+  }
+
+  onSelect(): any {
+    // console.table("hello category is selected:", this.categories)
+  }
+
+  private createSvg(): void {
+    const xValue = (d: { Gls: d3.NumberValue; }) => d.Gls
+    const yValue = (d: { Player: string; }) => d.Player
+    const margin = { top:20, right:20, bottom:20, left:100 };
+    const innerWidth = this.width- margin.left - margin.right;
+    const innerHeight = this.height - margin.top - margin.bottom;
+
+
+    this.svg = d3
+      .select('#vis2')
+      .append('svg')
+      .attr('width', this.width)
+      .attr('height', this.height)
+      .append('g')
+      .attr('id', 'vis2-svg');
+
+
+    const xScale = scaleLinear().domain([0,max(this.data, d => d.Gls + 8)])
+      .range([0,innerWidth]);
+    const yScale = scaleBand().domain(this.data.map(yValue))
+      .range([0,innerHeight])
+      .padding(0.4);
+
+    const g = this.svg.append('g')
+      .attr('transform', `translate(${margin.left},${margin.top})`)
+
+    g.append('g').call(axisLeft(yScale))
+    g.append('g').call(axisBottom(xScale))
+
+    g.selectAll('rect').data(this.data)
+      .enter().append('rect')
+      .attr('fill', '#5ccbf0')
+      .attr('y', (d: { Player: string; }) => yScale(yValue(d)))
+      .attr('width', (d: { Gls: d3.NumberValue; }) =>xScale(xValue(d)))
+      .attr('height',yScale.bandwidth());
+
+
+
   }
 
 }
+function steelblue(arg0: string, steelblue: any) {
+  throw new Error('Function not implemented.');
+}
+
