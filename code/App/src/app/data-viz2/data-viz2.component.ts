8f0ff62743817f4a901605e9763f618a1bde2188
Abderrahim Ammour <abderrahim223@gmail.com>
2022-04-06 07:54:57 -0400

    axis fix and style v2

--- code/App/src/app/data-viz2/data-viz2.component.ts
+++ code/App/src/app/data-viz2/data-viz2.component.ts
@@ -22,6 +22,7 @@ export class DataViz2Component implements OnInit {
     { value: 'formation', viewValue: 'Afficher le club de formation' },
     { value: 'club', viewValue: 'Afficher le club de actuelle' },
   ];
+  headers: string[] = [ 'Joueur Canadien', 'Joueur de la Concacaf'];
 
 
   constructor() { }
@@ -43,47 +44,63 @@ export class DataViz2Component implements OnInit {
   }
 
   private createSvg(): void {
+
+    // get data attribute from data file
     const xValue = (d: { Gls: d3.NumberValue; }) => d.Gls
+    const x2Value = (d: { Ast: d3.NumberValue; }) => d.Ast
     const yValue = (d: { Player: string; }) => d.Player
-    const margin = { top:20, right:20, bottom:20, left:100 };
+
+    // set the dimensions and margins of the graph
+    const margin = { top:70, right:20, bottom:20, left:150 };
     const innerWidth = this.width- margin.left - margin.right;
     const innerHeight = this.height - margin.top - margin.bottom;
 
-
+    // append the svg object to the body of the page
     this.svg = d3
       .select('#vis2')
       .append('svg')
       .attr('width', this.width)
       .attr('height', this.height)
       .append('g')
-      .attr('id', 'vis2-svg');
+      .attr('id', 'vis2-svg')
+      .attr("transform", `translate(0,0)`);
 
-
-    const xScale = scaleLinear().domain([0,max(this.data, d => d.Gls + 8)])
+    // Create X and Y axis DOMAIN and RANGE
+    const xScale = scaleLinear()
+      .domain([0,max(this.data, d => d.Gls + 8)])
       .range([0,innerWidth]);
-    const yScale = scaleBand().domain(this.data.map(yValue))
+    const yScale = scaleBand()
+      .domain(this.data.map(yValue))
       .range([0,innerHeight])
       .padding(0.4);
 
     const g = this.svg.append('g')
       .attr('transform', `translate(${margin.left},${margin.top})`)
 
-    g.append('g').call(axisLeft(yScale))
-    g.append('g').call(axisBottom(xScale))
+    // Custom axis X and Y
+    const xAxis = axisBottom(xScale).tickSize(innerHeight)
+    const yAxis = axisLeft(yScale)
+
+    // Append the Y Axis
+    g.append('g').attr('class', 'yAxis').call(yAxis)
+      .selectAll('.domain, .tick line').remove();
+    // Add the X Axis
+    g.append('g').attr('class', 'xAxis').call(xAxis)
+      .selectAll('.domain').remove();
+
+    const stackedData = d3.stack()(this.data)
 
     g.selectAll('rect').data(this.data)
       .enter().append('rect')
       .attr('fill', '#5ccbf0')
       .attr('y', (d: { Player: string; }) => yScale(yValue(d)))
       .attr('width', (d: { Gls: d3.NumberValue; }) =>xScale(xValue(d)))
-      .attr('height',yScale.bandwidth());
+      .attr('height',yScale.bandwidth())
 
 
-
+    g.append('text').attr('y', -30).text(`${this.headers[0]}`).attr('class','title-viz2')
   }
 
 }
-function steelblue(arg0: string, steelblue: any) {
-  throw new Error('Function not implemented.');
-}
+
 
